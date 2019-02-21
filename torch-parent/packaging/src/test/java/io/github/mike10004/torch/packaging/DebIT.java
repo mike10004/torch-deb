0a731bbb05e5aabef983917c7816dde8ac25290b
Mike Chaberski <mchaberski@novetta.com>
2019-02-20 19:39:28 -0500

    move around tests to allow repeat builds

--- torch-parent/packaging/src/test/java/io/github/mike10004/torch/packaging/DebIT.java
+++ torch-parent/packaging/src/test/java/io/github/mike10004/torch/packaging/DebIT.java
@@ -3,8 +3,38 @@ package io.github.mike10004.torch.packaging;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class DebIT {
+
+    @Test
+    public void stagingDirNonempty() throws Exception {
+        File stageDir = Utils.resolveDebStagingDir();
+        Path usrBinDir = stageDir.toPath().resolve("usr").resolve("bin");
+        if (!usrBinDir.toFile().isDirectory()) {
+            throw new FileNotFoundException("not a directory: " + usrBinDir);
+        }
+        Files.walk(usrBinDir, 1)
+                .map(Path::toFile)
+                .filter(File::isFile)
+                .forEach(file -> {
+                    if (Files.isSymbolicLink(file.toPath())) {
+                        try {
+                            file = file.toPath().toRealPath().toFile();
+                        } catch (IOException e) {
+                            fail(e.toString());
+                        }
+                    }
+                    assertTrue("file must be executable: " + file, file.canExecute());
+                });
+    }
 
     @Test
     public void debFileExists() throws Exception {
--- torch-parent/packaging/src/test/java/io/github/mike10004/torch/packaging/DebTest.java
+++ torch-parent/packaging/src/test/java/io/github/mike10004/torch/packaging/DebTest.java
@@ -3,32 +3,20 @@ package io.github.mike10004.torch.packaging;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertNotEquals;
 
 public class DebTest {
 
     @Test
-    public void usrBinFilesExecutable() throws Exception {
+    public void stagingDirNonempty() throws Exception {
         File stageDir = Utils.resolveDebStagingDir();
-        Path usrBinDir = stageDir.toPath().resolve("usr").resolve("bin");
-        Files.walk(usrBinDir, 1)
-                .map(Path::toFile)
+        long numFiles = Files.walk(stageDir.toPath()).map(Path::toFile)
                 .filter(File::isFile)
-                .forEach(file -> {
-                    if (Files.isSymbolicLink(file.toPath())) {
-                        try {
-                            file = file.toPath().toRealPath().toFile();
-                        } catch (IOException e) {
-                            fail(e.toString());
-                        }
-                    }
-                    assertTrue("file must be executable: " + file, file.canExecute());
-        });
+                .count();
+        assertNotEquals("expect nonzero file count in " + stageDir, 0, numFiles);
     }
 
 }
